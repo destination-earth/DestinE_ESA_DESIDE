51e44ef62bfc3ea81f8ec57cd8e56b4c38de985d
Stephan Meißl <stephan.meissl@eox.at>
2024-04-18 18:10:54 +0200

    adding SVVR stub

--- documents/bin/generate-docs.sh
+++ documents/bin/generate-docs.sh
@@ -23,7 +23,7 @@ echo '<!DOCTYPE html>
       <ul>' >> output/index.html
 
 
-DOCS="${BUILD_DOCS:-PMP UCD SRS SVVP SRP}"
+DOCS="${BUILD_DOCS:-PMP UCD SRS SVVP SVVR SRP}"
 
 for doc in $DOCS ; do
   echo "Building $doc"
