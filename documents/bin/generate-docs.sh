bb6952872324857723d2053830b5de1dfb2959b9
Tyna Dolezalova <tyna.dolezalova@eox.at>
2024-04-23 13:14:32 +0200

    add ucpp

--- documents/bin/generate-docs.sh
+++ documents/bin/generate-docs.sh
@@ -23,7 +23,7 @@ echo '<!DOCTYPE html>
       <ul>' >> output/index.html
 
 
-DOCS="${BUILD_DOCS:-PMP UCD SRS SVVP SVVR SRP SRF}"
+DOCS="${BUILD_DOCS:-PMP UCD SRS SVVP SVVR SRP SRF UCPP}"
 
 for doc in $DOCS ; do
   echo "Building $doc"
